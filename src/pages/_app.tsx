b3aba483d23d7127842ea08ae981a744887eb65c
Spiielberg <spiielberg@gmail.com>
2021-04-20 14:04:46 -0300

    mostrando componentes Header e Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,24 @@ import { AppProps } from 'next/app';
 
 import '../styles/global.scss';
 
+import styles from '../styles/app.module.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
 const MyApp: React.FC<AppProps> = ({
   Component,
   pageProps,
 }): React.ReactElement => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 };
 
 export default MyApp;
