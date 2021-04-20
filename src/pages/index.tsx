88465493e8c25a0c7a5a45fbeb80b6a7b5e4ff48
Spiielberg <spiielberg@gmail.com>
2021-04-20 14:29:39 -0300

    adicionando chamada a api de forma estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,3 +7,15 @@ const Home: React.FC = (): React.ReactElement => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
