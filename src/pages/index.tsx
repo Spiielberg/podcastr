9b964fa0f01782e4e757f707cc1db4ad4716748e
Spiielberg <spiielberg@gmail.com>
2021-04-21 14:03:56 -0300

    corrigindo link do banco de dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default Home;
 
 export async function getStaticProps() {
   const response = await fetch(
-    'https://my-json-server.typicode.com/Spiielberg/podcastr-server'
+    'https://my-json-server.typicode.com/Spiielberg/podcastr-server/episodes'
   );
   const data = await response.json();
 
